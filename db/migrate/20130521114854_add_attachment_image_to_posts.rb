b57670992e37b3ce7bd64d8e14b5727bda06a5d1
Arti Mathanda <artismarti@gmail.com>
2013-05-23 11:03:14 +0100

    updated migration for heroku

--- db/migrate/20130521114854_add_attachment_image_to_posts.rb
+++ db/migrate/20130521114854_add_attachment_image_to_posts.rb
@@ -1,11 +1,15 @@
 class AddAttachmentImageToPosts < ActiveRecord::Migration
   def self.up
-    change_table :posts do |t|
-      t.attachment :image
-    end
+    add_column :posts, :image_file_name, :string
+    add_column :posts, :image_content_type, :string
+    add_column :posts, :image_file_size, :integer
+    add_column :posts, :image_updated_at, :datetime
   end
 
   def self.down
-    drop_attached_file :posts, :image
+    remove_column :posts, :image_file_name
+    remove_column :posts, :image_content_type
+    remove_column :posts, :image_file_size
+    remove_column :posts, :image_updated_at
   end
 end
